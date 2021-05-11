5dcae5a29cdf8e8d97ebfbe5637a6b9c2912a6d9
JonneTyo <jojutam@utu.fi>
2021-05-11 14:41:47 +0300

    Small tweak to ease for adding more test methods.

--- R/r_test.R
+++ R/r_test.R
@@ -2,11 +2,11 @@ rm(list = ls())
 source("sensWeighted.R")
 data <- read.csv('generated_sens_spec.csv')
 data_y <- read.csv('test_results.csv')
-
+test_methods <- c('Jsens', 'Jsens2', 'Csens', 'Csens2', 'CPsens')
 result_frame <- data.frame(
-X = c('Jsens', 'Jsens2', 'Csens', 'Csens2', 'CPsens'),
-sens = 1:5,
-spec = 1:5
+X = test_methods,
+sens = 1:length(test_methods),
+spec = 1:length(test_methods)
 )
 
 
@@ -16,7 +16,7 @@ result_frame[result_frame$X=='Csens', c('sens', 'spec')] <- Csens(data$sens, dat
 result_frame[result_frame$X=='Csens2', c('sens', 'spec')] <- Csens2(data$sens, data$spec)[1:2]
 result_frame[result_frame$X=='CPsens', c('sens', 'spec')] <- CPsens(data$sens, data$spec)[1:2]
 
-temp <- merge(data_y, result_frame, by=c('X'))
+temp <- merge(data_y, result_frame, by='X')
 print('Sens test: ')
 abs(temp$sens.x - temp$sens.y) <= 1e-12
 print('Spec test: ')
