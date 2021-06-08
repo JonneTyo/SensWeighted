a348b61af51927d12c4cdbfcaafd872b27078bf6
JonneTyo <jojutam@utu.fi>
2021-06-08 08:53:19 +0300

    Changed method names from test scripts and their datas.

--- R/r_test.R
+++ R/r_test.R
@@ -2,7 +2,7 @@ rm(list = ls())
 source("sensWeighted.R")
 data <- read.csv('generated_sens_spec.csv')
 data_y <- read.csv('test_results.csv')
-test_methods <- c('Jsens', 'Jsens2', 'Csens', 'Csens2', 'CPsens')
+test_methods <- c('SenJ', 'SenLin', 'SenCirc', 'SenEll', 'SenConp')
 result_frame <- data.frame(
 X = test_methods,
 sens = 1:length(test_methods),
@@ -10,11 +10,11 @@ spec = 1:length(test_methods)
 )
 
 
-result_frame[result_frame$X=='Jsens', c('sens', 'spec')] <- Jsens(data$sens, data$spec)[1:2]
-result_frame[result_frame$X=='Jsens2', c('sens', 'spec')] <- Jsens2(data$sens, data$spec)[1:2]
-result_frame[result_frame$X=='Csens', c('sens', 'spec')] <- Csens(data$sens, data$spec)[1:2]
-result_frame[result_frame$X=='Csens2', c('sens', 'spec')] <- Csens2(data$sens, data$spec)[1:2]
-result_frame[result_frame$X=='CPsens', c('sens', 'spec')] <- CPsens(data$sens, data$spec)[1:2]
+result_frame[result_frame$X=='SenJ', c('sens', 'spec')] <- SenJ(data$sens, data$spec)[1:2]
+result_frame[result_frame$X=='SenLin', c('sens', 'spec')] <- SenLin(data$sens, data$spec)[1:2]
+result_frame[result_frame$X=='SenCirc', c('sens', 'spec')] <- SenCirc(data$sens, data$spec)[1:2]
+result_frame[result_frame$X=='SenEll', c('sens', 'spec')] <- SenEll(data$sens, data$spec)[1:2]
+result_frame[result_frame$X=='SenConp', c('sens', 'spec')] <- SenConp(data$sens, data$spec)[1:2]
 
 temp <- merge(data_y, result_frame, by='X')
 print('Sens test: ')
